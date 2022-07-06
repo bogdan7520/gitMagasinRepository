8ea5a398d44d06986d916b860885fb927799b39f
bogdan7520 <108740288+bogdan7520@users.noreply.github.com>
2022-07-06 15:00:22 +0300

    added colectstatic and deleted secret key

--- magasinproektdj/settings.py
+++ magasinproektdj/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'djangob-insecureb-y_jhcfb-nnj3f93b-dai65b*srqb#b*75ptt4b-ubb!mkxtsb#b*lbb-vs^r_secretn_keyn'
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ['DJANGO_DEBUG']
 
 ALLOWED_HOSTS = ['krizhma.herokuapp.com',]
 
