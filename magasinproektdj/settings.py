2881e19312b1d8b785f7b9272f62892a0e921b66
bogdan7520 <108740288+bogdan7520@users.noreply.github.com>
2022-07-06 09:37:55 +0300

    settings secret key changes env

--- magasinproektdj/settings.py
+++ magasinproektdj/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/etc/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
 
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
