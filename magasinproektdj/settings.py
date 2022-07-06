b9ca7202abc4d71ed3fe10d2c89c1e913000268d
bogdan7520 <108740288+bogdan7520@users.noreply.github.com>
2022-07-06 13:38:26 +0300

    delete import storage

--- magasinproektdj/settings.py
+++ magasinproektdj/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import whitenoise
 import os
-#from storages import storage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
